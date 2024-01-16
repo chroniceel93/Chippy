ce23c3ca54e31569171e2ac729b484efbb462427
ChronicEel93 <Wktradewell@gmail.com>
2024-01-16 01:53:08 -0600

    Actually print a message, and exit safely, if a ROM file is not provided.

--- chipper.cpp
+++ chipper.cpp
@@ -7,13 +7,18 @@ int selftest() {
 int main(int argc, char *argv[]) {
     tehCHIP b;
     std::string rom_path;
-    try {
-        b.load_program(argv[1]);
-        b.execute();
-    } catch (const std::out_of_range &e) {
-        std::cout << e.what() << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
+
+    if (argc == 1) {
+        std::cout << "Rom file not specified.\r\n";
+    } else {
+        try {
+            b.load_program(argv[1]);
+            b.execute();
+        } catch (const std::out_of_range &e) {
+            std::cout << e.what() << std::endl;
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
     }
 
 
