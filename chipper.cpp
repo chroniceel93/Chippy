52b4eb872eb889d56ae1ae9156d340ef0a564324
chroniceel93 <wktradewell@gmail.com>
2024-11-04 23:38:32 -0600

    Added error handling, and verbose handling for NFD lib.

--- chipper.cpp
+++ chipper.cpp
@@ -1,6 +1,6 @@
 #include "chipper.h"
-
 #include <nfd.h>
+#include <iostream>
 
 #ifdef _WIN32
 int WinMain(int argc, char *argv[]) {
@@ -9,20 +9,28 @@ int main(int argc, char *argv[]) {
 #endif
     tehCHIP b;
     nfdchar_t *outPath = NULL;
-    nfdresult_t result = NFD_OpenDialog( "ch8", NULL, &outPath);
+    nfdresult_t result = NFD_OpenDialog("ch8", NULL, &outPath);
 
-    if (result == NFD_OKAY) {
-        try {
-            b.load_program(outPath);
-            b.execute();
-        } catch (const std::out_of_range &e) {
-            std::cout << e.what() << std::endl;
-        } catch (const std::exception &e) {
-            std::cout << e.what() << std::endl;
-        }
-    } // All other cases should exit. We either have a ROM, or we do not.
+    switch (result) {
+        case NFD_OKAY:
+            try {
+                b.load_program(outPath);
+                b.execute();
+            } catch (const std::out_of_range &e) {
+                std::cout << "Out of range error: " << e.what() << std::endl;
+            } catch (const std::exception &e) {
+                std::cout << "Exception: " << e.what() << std::endl;
+            }
+            break;
+        case NFD_CANCEL:
+            std::cout << "User pressed cancel." << std::endl;
+            break;
+        case NFD_ERROR:
+            std::cout << "Error: " << NFD_GetError() << std::endl;
+            break;
+    }
 
-// Original main, pre NFD
+    // Original main, pre NFD
 
     // if (argc == 1) {
     //     std::cout << "Rom file not specified.\r\n";
@@ -36,7 +44,6 @@ int main(int argc, char *argv[]) {
     //         std::cout << e.what() << std::endl;
     //     }
     // }
-
 
     return 0;
 }
