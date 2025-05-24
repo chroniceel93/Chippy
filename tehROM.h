86a256c85af7aaf27e9fc2d014235c95ae02048e
chroniceel93 <wktradewell@gmail.com>
2025-05-23 23:14:52 -0500

    Documented tehROM

--- tehROM.h
+++ tehROM.h
@@ -8,24 +8,51 @@
 #include <stdexcept>
 #include <string>
 
+/**
+ * @class tehROM
+ * @brief A class for reading a file into memory in chunks.
+ */
 class tehROM {
 private:
-    static const short int chunkSize = 256;
-    long fileSize; // Size of file, in bytes
-    int chunkTotal; // Total number of chunks of size chunkSize
-    int currentChunk; // Current Chunk.
-    std::fstream file;
-    std::string fileName;
+    static const short int chunkSize = 256; /**< Size of each chunk to be read from the file. */
+    long fileSize; /**< Size of the file, in bytes. */
+    int chunkTotal; /**< Total number of chunks of size chunkSize. */
+    int currentChunk; /**< Index of the current chunk being read. */
+    std::fstream file; /**< File stream object used to open and read the file. */
+    std::string fileName; /**< Name of the file to be read. */
 public:
+    /**
+     * @brief Default constructor.
+     */
     tehROM();
+
+    /**
+     * @brief Constructor that initializes the object with a file.
+     * @param filename The name of the file to be read.
+     */
     tehROM(std::string filename);
 
+    /**
+     * @brief Destructor.
+     */
     ~tehROM();
 
+    /**
+     * @brief Determines the size of the file and calculates the total number of chunks.
+     * @param filename The name of the file.
+     */
     void read_file_size(std::string filename);
 
+    /**
+     * @brief Checks if the end of the file has been reached.
+     * @return true if the end of the file has been reached, false otherwise.
+     */
     bool get_eof();
 
+    /**
+     * @brief Reads the next chunk of data from the file.
+     * @return The next chunk as a string.
+     */
     std::string read_next_chunk();
 };
 
