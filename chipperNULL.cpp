a849f2f21d8897efb040f79c96205d82d0670eea
chroniceel93 <wktradewell@gmail.com>
2026-03-25 19:06:04 -0500

    Updated chipperNULL to match the updated virtual interfaces

--- chipperNULL.cpp
+++ chipperNULL.cpp
@@ -20,22 +20,34 @@ int chipperNULL::get_height() {
 	return 32;
 }
 
-void chipperNULL::SoundTick(bool mute) {
+void chipperNULL::copy_audio(uint8_t* data, int size) {
 	return;
+}
+
+int chipperNULL::get_sample_rate() {
+	return 48000; // use sane default so math doesn't blow up
+}
+
+int chipperNULL::get_bytes_per_sample() {
+	return 2;
+}
+
+int chipperNULL::get_buffer_size() {
+	return 0; 
 }
 
 void chipperNULL::process_events() {
 	return;
 }
 
-bool chipperNULL::get_exit_state() {
+bool chipperNULL::get_exit_state() const {
 	return false;
 }
 
-bool chipperNULL::is_key_pressed(unsigned char value) {
+bool chipperNULL::is_key_pressed(unsigned char value) const {
 	return false;
 }
 
-unsigned char chipperNULL::get_key_pressed() {
+unsigned char chipperNULL::get_key_pressed() const {
 	return ' ';
 }
--- chipperNULL.h
+++ chipperNULL.h
@@ -15,12 +15,15 @@ public:
 	int get_width();
 	int get_height();
 
-	void SoundTick(bool mute);
+	void copy_audio(uint8_t* data, int size);
+	int get_sample_rate();
+	int get_bytes_per_sample();
+	int get_buffer_size();
 
 	void process_events();
-	bool get_exit_state();
-	bool is_key_pressed(unsigned char value);
-	unsigned char get_key_pressed();
+	bool get_exit_state() const;
+	bool is_key_pressed(unsigned char value) const;
+	unsigned char get_key_pressed() const;
 };
 
 #endif
