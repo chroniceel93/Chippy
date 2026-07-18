6c4267303cf8fddb1f55aa800725ecca4625fa13
chroniceel93 <wktradewell@gmail.com>
2026-07-18 17:03:30 -0500

    Stub out tehGUI functions in chipperNULL

--- chipperNULL.cpp
+++ chipperNULL.cpp
@@ -36,7 +36,7 @@ int chipperNULL::get_buffer_size() {
 	return 0; 
 }
 
-void chipperNULL::process_events() {
+void chipperNULL::process_keypad_events() {
 	return;
 }
 
@@ -50,4 +50,40 @@ bool chipperNULL::is_key_pressed(unsigned char value) const {
 
 unsigned char chipperNULL::get_key_pressed() const {
 	return ' ';
+}
+
+void chipperNULL::load_rom(std::string filename) {
+    return;
+}
+
+void chipperNULL::system_pause() {
+    return;
+}
+
+void chipperNULL::system_resume() {
+    return;
+}
+
+chippy::systype chipperNULL::get_quirks_mode() {
+    return chippy::CHIP8;
+}
+
+void chipperNULL::set_quirks_mode(chippy::systype quirks_mode) {
+    return;
+}
+
+int chipperNULL::get_interpreter_speed() {
+    return 0;
+}
+
+void chipperNULL::set_interpreter_speed(int interpreter_speed) {
+    return;
+}
+
+void chipperNULL::process_gui_events() {
+    return;
+}
+
+bool chipperNULL::get_exit_state() {
+    return false;
 }
--- chipperNULL.h
+++ chipperNULL.h
@@ -6,8 +6,9 @@
 #include "tehSCREEN.h"
 #include "tehBOOP.h"
 #include "tehBEEP.h"
+#include "tehGUI.h"
 
-class chipperNULL: public tehSCREEN, public tehBOOP, public tehBEEP {
+class chipperNULL: public tehSCREEN, public tehBOOP, public tehBEEP, public tehGUI {
 public:
 	void copy_screen(bool* data, int size);
 	void refresh_screen();
@@ -20,10 +21,20 @@ public:
 	int get_bytes_per_sample();
 	int get_buffer_size();
 
-	void process_events();
+	void process_keypad_events();
 	bool get_exit_state() const;
 	bool is_key_pressed(unsigned char value) const;
 	unsigned char get_key_pressed() const;
+
+	void load_rom(std::string filename);
+	void system_pause();
+	void system_resume();
+	chippy::systype get_quirks_mode();
+	void set_quirks_mode(chippy::systype quirks_mode);
+	int get_interpreter_speed();
+	void set_interpreter_speed(int interpreter_speed);
+	void process_gui_events();
+	bool get_exit_state();
 };
 
 #endif
